cbd4322cd69be906e761b70ab4336f10a7f05381
Joao veras <joaohg.veras@gmail.com>
2016-05-05 12:42:46 -0400

    user info with bugs and interface changes

--- constants.php
+++ constants.php
@@ -1,6 +1,7 @@
 <?php 
 
-	define('SERVERNAME', 'localhost:3306');
+	// define('SERVERNAME', 'localhost:3306');
+	define('SERVERNAME', 'localhost');
 	define('USERNAME', 'proj6');
 	define('PASSWORD', 'brasil2016');
 	define('DBNAME', 'proj6');
--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php 
 	session_start();
 	if (!isset($_SESSION['logged_in'])){
-		// header( 'Location: login.php' ) ;
+		header( 'Location: login.php' ) ;
 	}
 
 	require_once("db.php");
 	require_once("utils.php");
-
+	
 	?>
 <!DOCTYPE html>
 <html lang="en">
